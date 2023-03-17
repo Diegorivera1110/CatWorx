9c9240a81f69c6161dd8960a8b90b2fed623d55e
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-03-16 19:23:07 -0700

    fixed name entering loop

--- Program.cs
+++ Program.cs
@@ -10,27 +10,20 @@ namespace CatWorx.BadgeMaker
             List<string> employees = new List<string>();
             // Collect user values until the value is an empty string
             while (true)
+        {
+            Console.WriteLine("Please enter a name: ");
+            string input = Console.ReadLine() ?? "";
+            // Break if the user hits ENTER without typing a name
+            if (input == "")
             {
-                Console.WriteLine("Please enter a name: ");
-                // Get a name from the console and assign it to a variable
-                string input = Console.ReadLine() ?? "";
-                employees.Add(input);
+                break;
+            }
+            employees.Add(input);
             }
             for (int i = 0; i < employees.Count; i++)
             {
                 Console.WriteLine(employees[i]);
-            }
-
-            while (true)
-            {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
-                string input = Console.ReadLine() ?? "";
-                // Break if the user hits ENTER without typing a name
-                if (input == "")
-                {
-                    break;
-                }
-            }
+        }
         }
     }
 }
