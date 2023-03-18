6433bfd3850716ed5599dd552bc884adb1dd6ac6
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-03-17 20:47:47 -0700

    added get employees and pring employees methods

--- Program.cs
+++ Program.cs
@@ -5,25 +5,37 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<string> GetEmployees()
         {
             List<string> employees = new List<string>();
-            // Collect user values until the value is an empty string
             while (true)
-        {
-            Console.WriteLine("Please enter a name: ");
-            string input = Console.ReadLine() ?? "";
-            // Break if the user hits ENTER without typing a name
-            if (input == "")
             {
-                break;
+
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+
+                string input = Console.ReadLine() ?? "";
+
+                if (input == "")
+                {
+                    break;
+                }
+                employees.Add(input);
             }
-            employees.Add(input);
-            }
+            return employees;
+        }
+
+        static void PrintEmployees(List<string> employees)
+        {
             for (int i = 0; i < employees.Count; i++)
             {
                 Console.WriteLine(employees[i]);
+            }
         }
+
+        static void Main(string[] args)
+        {
+            List<string> employees = GetEmployees();
+            PrintEmployees(employees);
         }
     }
 }
