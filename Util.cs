56b412246a8fd2a9b0bf2a0213fea4ab352c2fd9
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-03-29 20:31:36 -0700

    added printEmployees for loop to Util

--- Util.cs
+++ Util.cs
@@ -2,9 +2,13 @@ namespace CatWorx.BadgeMaker
 {
     class Util
     {
-        public static void PrintEmployees()
+        public static void PrintEmployees(List<Employee> employees)
         {
-            
+            for (int i = 0; i < employees.Count; i++)
+            {
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+            }
         }
     }
 }
