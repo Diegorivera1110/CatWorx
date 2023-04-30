d4fd0a5f01f746983f3d986b295047170cc97575
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-04-30 08:49:04 -0700

    transferring GetEmployees to PeopleFetcher

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -6,5 +6,31 @@ namespace CatWorx.BadgeMaker
     class PeopleFetcher
     {
         // code from getEmplpyees() in Program.cs
+        static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                
+                Console.WriteLine("Enter first name (leave empty to exit): ");
+                string firstName = Console.ReadLine() ?? "";
+                if (firstName == "")
+                {
+                    break;
+                }
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine() ?? "";
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine() ?? "");
+                Console.Write("Enter Photo URL:");
+                string photoUrl = Console.ReadLine() ?? "";
+                // Create a new Employee instance
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+            }
+            return employees;
+        }
+
+        public static void GetFromApi() {}
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,34 +6,34 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static List<Employee> GetEmployees()
-        {
-            List<Employee> employees = new List<Employee>();
-            while (true)
-            {
+        // static List<Employee> GetEmployees()
+        // {
+        //     List<Employee> employees = new List<Employee>();
+        //     while (true)
+        //     {
                 
-                Console.WriteLine("Enter first name (leave empty to exit): ");
-                string firstName = Console.ReadLine() ?? "";
-                if (firstName == "")
-                {
-                    break;
-                }
-                Console.Write("Enter last name: ");
-                string lastName = Console.ReadLine() ?? "";
-                Console.Write("Enter ID: ");
-                int id = Int32.Parse(Console.ReadLine() ?? "");
-                Console.Write("Enter Photo URL:");
-                string photoUrl = Console.ReadLine() ?? "";
-                // Create a new Employee instance
-                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-                employees.Add(currentEmployee);
-            }
-            return employees;
-        }
+        //         Console.WriteLine("Enter first name (leave empty to exit): ");
+        //         string firstName = Console.ReadLine() ?? "";
+        //         if (firstName == "")
+        //         {
+        //             break;
+        //         }
+        //         Console.Write("Enter last name: ");
+        //         string lastName = Console.ReadLine() ?? "";
+        //         Console.Write("Enter ID: ");
+        //         int id = Int32.Parse(Console.ReadLine() ?? "");
+        //         Console.Write("Enter Photo URL:");
+        //         string photoUrl = Console.ReadLine() ?? "";
+        //         // Create a new Employee instance
+        //         Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+        //         employees.Add(currentEmployee);
+        //     }
+        //     return employees;
+        // }
 
         async static Task Main(string[] args)
         {
-            List<Employee> employees = GetEmployees();
+            List<Employee> employees = PeopleFetcher.GetEmployees();
             Util.PrintEmployees(employees);
             Util.MakeCSV(employees);
             await Util.MakeBadges(employees);
