42e52e5c5e5b9141a8eddf67214e7430b3b29ece
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2023-05-02 16:48:39 -0700

    temporary commenting out List<Employee> for API testing purposes

--- PeopleFetcher.cs
+++ PeopleFetcher.cs
@@ -9,25 +9,6 @@ namespace CatWorx.BadgeMaker
         static List<Employee> GetEmployees()
         {
             List<Employee> employees = new List<Employee>();
-            while (true)
-            {
-                
-                Console.WriteLine("Enter first name (leave empty to exit): ");
-                string firstName = Console.ReadLine() ?? "";
-                if (firstName == "")
-                {
-                    break;
-                }
-                Console.Write("Enter last name: ");
-                string lastName = Console.ReadLine() ?? "";
-                Console.Write("Enter ID: ");
-                int id = Int32.Parse(Console.ReadLine() ?? "");
-                Console.Write("Enter Photo URL:");
-                string photoUrl = Console.ReadLine() ?? "";
-                // Create a new Employee instance
-                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-                employees.Add(currentEmployee);
-            }
             return employees;
         }
 
